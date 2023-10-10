8517c0e30c57be7eb327ee041d0091a65d7099e3
Daniel Wu <danielwu@Daniels-MacBook-Pro.local>
2023-10-10 11:03:54 -0400

    adding keys

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+// hide keys from repo
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config() // loads all variables in .env and puts in process var
+}
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+
 const express = require('express')
 const app = express()
 
